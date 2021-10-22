8360e96e2e60b11e1466c5aca6f2063e304b283a
Pamela Hammond <hammondpamelac@gmail.com>
2021-10-22 13:05:00 -0400

    changed navtabs and added css

--- react-portfolio/src/components/PortfolioContainer.js
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -8,7 +8,6 @@ import ContactPage from "./pages/ContactPage";
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("Home");
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === "Home") {
       return <HomePage />;
@@ -26,9 +25,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>
   );
--- react-portfolio/src/components/pages/HomePage.js
+++ react-portfolio/src/components/pages/HomePage.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../Header";
 import NameCard from "../NameCard";
 import Footer from "../Footer";
 
 const HomePage = () => {
   return (
     <>
-      <Header />
       <NameCard />
       <Footer />
     </>
