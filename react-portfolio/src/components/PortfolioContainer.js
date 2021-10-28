38e78c11b8b199b2a0a5e9cd1fe83ab7ce905a22
Pamela Hammond <hammondpamelac@gmail.com>
2021-10-27 21:43:46 -0400

    moved pages folder to src and changed minheight

--- react-portfolio/src/components/PortfolioContainer.js
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -24,9 +24,9 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div>
+    <main>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
-    </div>
+    </main>
   );
 }
