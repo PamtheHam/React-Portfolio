fa82eebf94353f61f6ef7dfcf08d6e281e22179e
Pamela Hammond <hammondpamelac@gmail.com>
2021-10-27 21:46:13 -0400

    changed file paths in portfoliocontainer.js

--- react-portfolio/src/components/PortfolioContainer.js
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import NavTabs from "./NavTabs";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import PortfolioPage from "./pages/PortfolioPage";
-import ContactPage from "./pages/ContactPage";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import PortfolioPage from "../pages/PortfolioPage";
+import ContactPage from "../pages/ContactPage";
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("Home");
